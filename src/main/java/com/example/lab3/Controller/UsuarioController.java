8951a4ca2c30b02980024efbf18237d6f573c82e
Miguel <a20190050@pucp.edu.pe>
2022-09-20 20:43:54 -0500

    request mapping usuario

--- src/main/java/com/example/lab3/Controller/UsuarioController.java
+++ src/main/java/com/example/lab3/Controller/UsuarioController.java
@@ -1,4 +1,13 @@
 package com.example.lab3.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+
+
+
 }
