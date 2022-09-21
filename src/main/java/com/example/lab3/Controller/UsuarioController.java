8f9bdc4d0891ac191e57cdb0a38b58661dc1acce
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-20 20:59:34 -0500

    Empleados

--- src/main/java/com/example/lab3/Controller/UsuarioController.java
+++ src/main/java/com/example/lab3/Controller/UsuarioController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UsuarioController {
     @GetMapping("")
     String listaUsuario(){
-        return "Empleados/newForm";
+        return "Empleados/NewForm";
     }
 }
