53cdd9ce62c4e7ade30c941e2a0778741159f205
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-20 20:52:58 -0500

    Capeta Empleados

--- src/main/java/com/example/lab3/Controller/UsuarioController.java
+++ src/main/java/com/example/lab3/Controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.example.lab3.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/usuario")
+@RequestMapping("/empleado")
 public class UsuarioController {
+    @GetMapping("")
+    String listaUsuario(){
+        return "Empleados/newForm";
+    }
 }
