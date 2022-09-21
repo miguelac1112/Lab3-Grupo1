a75add9ee5bab24794d610a5c7506f095ca1cd13
ArturoManuel <noriegaarturonoriega@gmail.com>
2022-09-20 21:54:41 -0500

    lista

--- src/main/java/com/example/lab3/Repository/EmployeeRepository.java
+++ src/main/java/com/example/lab3/Repository/EmployeeRepository.java
@@ -3,11 +3,17 @@ package com.example.lab3.Repository;
 import com.example.lab3.Dto.EmployeeDto;
 import com.example.lab3.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    @Modifying
+    @Transactional
+    @Query(value="update employees set password=SHA2(?1,256) where employee_id=?2 ",nativeQuery = true)
+    void GuardarContrasena(String contrasena,int id);
 
     @Query(value="SELECT e.employee_id, e.first_name, e.last_name, e.email, j.job_title, e.salary, \n" +
             "\t\t l.city, d.department_name\n" +
@@ -20,4 +26,10 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
             "    inner join employees m on (e.manager_id = m.employee_id)\n" +
             "order by employee_id;\n",nativeQuery = true)
     List<EmployeeDto> obtenerEmpleados();
+
+    @Query(value="SELECT * FROM employees group by manager_id",nativeQuery = true)
+    List <Employee> buscaJefes();
+
+
+
 }
